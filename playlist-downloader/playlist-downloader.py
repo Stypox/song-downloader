f179aadb3b7399aae47682c6a2d5315d9896a744
Stypox <stypox@pm.me>
2018-07-14 13:30:02 +0200

    Added command line and file arguments parsing.

--- playlist-downloader/playlist-downloader.py
+++ playlist-downloader/playlist-downloader.py
@@ -32,21 +32,18 @@ HEADERS = {
 	"Accept-Encoding": "none",
 	"Accept-Language": "en-US,en;q=0.8",
 	"Connection": "keep-alive"
-	}
+}
 
 INVALID_FILENAME_CHARS = "<>:\"/\\|?*"
 VALID_CHARS_MIN = 31 #char 31
 DOS_NAMES = ["CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9"]
 INVALID_SONG_NAME = "invalid_song_name.mp3"
 
-PLAYLIST_LINK = "https://www.youtube.com/playlist?list={}"
-VIDEO_LINK = "https://youtu.be/{}"
-CONVERTER_VIDEO_LINK = "https://www.convertmp3.io/download/?video=https://www.youtube.com/watch?v={}"
-CONVERTER_EMPTY_LINK = "https://www.convertmp3.io{}"
+VIDEO_ID_LEN = 11
+PLAYLIST_ID_LEN = 34
 
-PLAYLIST_ID_FILE_NAME = "playlist.txt"
-DEVELOPER_KEY_FILE_NAME = "devkey.txt"
-DOWNLOADED_IDS_FILE_NAME = "downloaded.txt"
+DEVELOPER_KEY_FILE_NAME = "playlist-downloader-devkey.txt"
+IDS_FILE_NAME = "playlist-downloader-ids.txt"
 
 TIME_PAUSE_IF_ERROR = 10 #seconds
 TIME_BEFORE_RESTARTING = 30 #seconds
@@ -60,18 +57,10 @@ def authenticateYt():
 		return authentication
 
 	except HttpError as e:
-		print("An HTTP error ({}) occurred while authenticating: {}".format(e.resp.status, e.content))
+		print("An HTTP error (%s) occurred while authenticating: %s" % (e.resp.status, e.content))
 		return None
 	except FileNotFoundError as e:
-		print("File named \"devkey.txt\" containing a developer key doesn't exists")
-		return None
-def readPlaylistId():
-	try:
-		for line in open(PLAYLIST_ID_FILE_NAME, "r"): playlistLink = line
-		return playlistLink
-
-	except FileNotFoundError:
-		print("File named \"playlist.txt\" containing a playlist id doesn't exists")
+		print("File named \"%s\" containing a developer key doesn't exists" % DEVELOPER_KEY_FILE_NAME)
 		return None
 def downloadPage(link):
 	linkHeader = urllib.request.Request(link, headers=HEADERS)
@@ -171,7 +160,11 @@ class Video:
 			self.playlistIndex = playlistIndex
 			self.song = Song(title)
 	def __repr__(self):
-		return self.song.filename
+		if self.song is None:
+			return self.Id
+		else:
+			return self.song.filename
+			
 	def loadData(self):
 		if self.song is not None:
 			raise RuntimeError("Error: trying to run loadData() on a video (id: %s) that already has data loaded (title: %s)" % (self.Id, self.title))
@@ -179,7 +172,7 @@ class Video:
 			ytVidResponse = Video.ytAgent.videos().list(id=self.Id, part="snippet").execute()
 			self.song = Song(ytVidResponse["items"][0]["snippet"]["title"])
 		except HttpError as e:
-			print("An HTTP error ({}) occurred while retrieving videos from playlist ({}): {}".format(e.resp.status, self.Id, e.content))
+			print("An HTTP error (%s) occurred while retrieving video title of %s: %s" % (e.resp.status, self.Id, e.content))
 			return False
 		return True
 	def updateFile(self, directoryFilenames = None):
@@ -201,8 +194,8 @@ class Video:
 		else:
 			try:
 				if directoryFilenames[self.Id] != self.song.filename:
-					print("Song %s changed name: renaming to %s" % (directoryFilenames[self.Id], self.song.filename))
-					os.rename(directoryFilenames[self.Id], self.song.filename)
+					print("Song %s changed name: renaming to %s" % (self.directory + directoryFilenames[self.Id], self.directory + self.song.filename))
+					os.rename(self.directory + directoryFilenames[self.Id], self.directory + self.song.filename)
 			except KeyError: pass
 	def saveToFile(self, data):
 		songFile = open(self.directory + self.song.filename, "wb")
@@ -212,9 +205,9 @@ class Video:
 		if self.song is None:
 			raise RuntimeError("Error: trying to download a video (id: %s) not belonging to a playlist before running loadData()" % self.Id)
 		if self.song.isValid(self.directory):
-			print("%s already downloaded to %s" % (self.Id, self.directory + self.song.filename))
+			print("%s already downloaded to %s... " % (self.Id, self.directory + self.song.filename), end = "", flush=True)
 			return True
-		print("Downloading %s to %s" % (self.Id, self.directory + self.song.filename))
+		print("Downloading %s to %s... " % (self.Id, self.directory + self.song.filename), end = "", flush=True)
 
 		infoLink = Video.converterLink.format(self.Id)
 		infoData = str(downloadPage(infoLink))
@@ -248,7 +241,7 @@ class Video:
 class Playlist:
 	ytAgent = None
 
-	def __init__(self, Id):
+	def __init__(self, Id, directory = None):
 		self.nr = 0
 		self.videos = []
 		if len(Id) == 34:
@@ -256,13 +249,19 @@ class Playlist:
 		else:
 			print("Playlist id must be 34 chars, not %i: %s" % (len(Id), Id))
 			self.Id = None
-		self.directory = "./" + Id + "/"
-		if not os.path.exists(self.directory):
-			os.makedirs(self.directory)
+		if directory is None:
+			self.directory = "./" + Id + "/"
+		else:
+			self.directory = directory
+			if self.directory[-1] != "/":
+				self.directory += "/"
 	def __getitem__(self, key):
 		return self.videos[key]
 	def __repr__(self):
-		return [video.__repr__() for video in self.videos].__repr__()
+		if len(self.videos) == 0:
+			return self.Id
+		else:
+			return [video.__repr__() for video in self.videos].__repr__()
 
 	def ready(self):
 		return Playlist.ytAgent is not None and self.Id is not None
@@ -280,10 +279,13 @@ class Playlist:
 					self.append(item["snippet"]["resourceId"]["videoId"], item["snippet"]["title"])
 				ytPlLister = ytPlaylist.list_next(ytPlLister, ytPlResponse)
 		except HttpError as e:
-			print("An HTTP error ({}) occurred while retrieving videos from playlist ({}): {}".format(e.resp.status, self.Id, e.content))
+			print("An HTTP error (%s) occurred while retrieving videos from playlist (%s): %s" % (e.resp.status, self.Id, e.content))
 			return False
 		return True
 	def download(self):
+		if not os.path.exists(self.directory):
+			os.makedirs(self.directory)
+
 		directoryFilenames = dict()
 		files = os.listdir(self.directory)
 		for filename in files:
@@ -292,64 +294,99 @@ class Playlist:
 				directoryFilenames[songFile["albumartist"][0]] = filename
 			except: continue
 
-		print(directoryFilenames)
-
 		for video in self.videos:
 			video.updateFile(directoryFilenames)
-			if not video.download():
-				print("Failed to download song %s" % video.Id)
-			if not video.saveMetadata():
-				print("Failed to save metadata on song %s" % video.Id)
-			print()
+			if video.download():
+				if video.saveMetadata():
+					print("Done")
+				else:
+					print("Failed to save metadata")
+			else:
+				print("Failed to download")
 
 
-# def main():
-	# 	success = []
-	# 	playlistLink = getPlaylistLink(success)
-	# 	if len(success) is not 0: return
-	# 	youtube = authenticate(success)
-	# 	if len(success) is not 0: return
-	# 	videos = getVideos(youtube, playlistLink, success)
-	# 	if len(success) is not 0: return
-	# 	while 1:
-	# 		downloadedIds = []
-	# 		fileDownloaded = open(DOWNLOADED_IDS_FILE_NAME, "r")
-	# 		for line in fileDownloaded:
-	# 			downloadedIds.append(line[:-1])
-	# 		fileDownloaded.close()
-	# 		print("Already downloaded videos:    ", end = "")
-	# 		print(downloadedIds, end = "\n\n\n")
-	# 		fileDownloaded = open(DOWNLOADED_IDS_FILE_NAME, "a")
-	# 		for i in range(0, videos.nr):
-	# 			videoPath = toMp3Path(videos.songArtists[i], videos.songTitles[i])
-	# 			if videos.ids[i] in downloadedIds:
-	# 				setMp3Metadata(videoPath, i + 1, videos.songTitles[i], videos.songArtists[i], videos.playlistId, videos.ids[i])
-	# 			else:
-	# 				if downloadVideo(videoPath, videos.ids[i], True):
-	# 					setMp3Metadata(videoPath, i + 1, videos.songTitles[i], videos.songArtists[i], videos.playlistId, videos.ids[i])
-	# 					fileDownloaded.write(videos.ids[i] + "\n")
-	# 					fileDownloaded.flush()
-	# 				#else:
-	# 					#os.remove(videoPath)
-	# 		fileDownloaded.close()
-	# 		break
+def parseArguments(tmpArgs, arguments):
+	if len(tmpArgs) == 0:
+		return None
+	elif len(tmpArgs) == 1:
+		if len(tmpArgs[0]) == VIDEO_ID_LEN:
+			return Video(tmpArgs[0])
+		elif len(tmpArgs[0]) == PLAYLIST_ID_LEN:
+			return Playlist(tmpArgs[0])
+		else:
+			raise RuntimeError("Invalid arguments (argument \"%s\" is neither a video nor a playlist id) \"%s\"" % (tmpArgs[0], arguments))
+	elif len(tmpArgs) == 2:
+		if not os.path.isdir(tmpArgs[1]):
+			print("%s is not an existing directory, it will be cerated" % (tmpArgs[1]))
+		if len(tmpArgs[0]) == VIDEO_ID_LEN:
+			return Video(tmpArgs[0], tmpArgs[1])
+		elif len(tmpArgs[0]) == PLAYLIST_ID_LEN:
+			return Playlist(tmpArgs[0], tmpArgs[1])
+		else:
+			raise RuntimeError("Invalid arguments (argument 1 of list \"%s\" is neither a video nor a playlist id) \"%s\"" % (tmpArgs, arguments))
+	else:
+		raise RuntimeError("Invalid arguments (list of arguments \"%s\" too long) \"%s\"" % (tmpArgs, arguments))
 
-def main():
+def main(arguments):
 	Video.ytAgent = Playlist.ytAgent = authenticateYt()
-	# vid = Video("rVAMYUSnR0I")
-	# vid.loadData()
-	# vid.updateFile()
-	# vid.download()
-	# vid.saveMetadata()
-	playlist = Playlist(readPlaylistId())
-	if not playlist.loadData(): return
+	if Playlist.ytAgent is None:
+		return
 
-	print(*playlist, sep = "\n")
+	#arguments parsing
+	videos = []
+	playlists = []
+	if len(arguments) > 1:
+		print("Parsing command line arguments... ", end = "", flush = True)
+		tmpArgs = []
+		args = arguments[1:]
+		for arg in args:
+			if arg == "-":
+				downloader = parseArguments(tmpArgs, args)
+				if type(downloader) is Video:
+					videos.append(downloader)
+				elif type(downloader) is Playlist:
+					playlists.append(downloader)
+				tmpArgs = []
+			else:
+				tmpArgs.append(arg)
+		downloader = parseArguments(tmpArgs, args)
+		if type(downloader) is Video:
+			videos.append(downloader)
+		elif type(downloader) is Playlist:
+			playlists.append(downloader)
+		print("Done")
+	else:
+		print("Reading and parsing file %s... " % IDS_FILE_NAME, end = "", flush = True)
+		idsFile = open(IDS_FILE_NAME, "r")
+		for arg in idsFile:
+			downloader = parseArguments(arg.split(' '), arguments)
+			if type(downloader) is Video:
+				videos.append(downloader)
+			elif type(downloader) is Playlist:
+				playlists.append(downloader)
+		print("Done")
 
-	playlist.download()
+	print("Videos:", *videos, "- Playlists:", *playlists)
+
+	#downloading
+	for video in videos:
+		video.loadData()
+		video.updateFile()
+		if video.download():
+			if video.saveMetadata():
+				print("Done")
+			else:
+				print("Failed to save metadata")
+		else:
+			print("Failed to download")
+	for playlist in playlists:
+		playlist.loadData()
+		playlist.download()
+
+
 
 
 if __name__ == "__main__":
-	print("\n-----\nSTART\n-----\n")
-	main()
-	print("\n-----\n END \n-----\n")
+	print("\n%s\nSTART %s\n%s\n" % ("-" * (6 + len(sys.argv[0])), sys.argv[0], "-" * (6 + len(sys.argv[0]))))
+	main(sys.argv)
+	print("\n%s\n END %s \n%s\n" % ("-" * (6 + len(sys.argv[0])), sys.argv[0], "-" * (6 + len(sys.argv[0]))))
